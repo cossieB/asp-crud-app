990410fd59fdfb03ce89cd4ef321aed262b1376c
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-04-19 23:19:10 +0200

    added create functionality

---
+++ Controllers/PublisherController.cs
@@ -0,0 +1,36 @@
+﻿using IGDB.Data;
+using IGDB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IGDB.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public PublisherController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Publisher> publishers = this._db.Publishers;
+            return View(publishers);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                this._db.Publishers.Add(publisher);
+                this._db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(publisher);
+        }
+    }
+}
