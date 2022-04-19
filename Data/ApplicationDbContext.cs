b3f2bf43872c43d9038b422e67a6119c6cc9e00b
Buntu Cossie <buntucossie@gmail.com>
2022-04-19 17:13:20 +0200

    configured DB

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IGDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IGDB.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Developer> Developers { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
