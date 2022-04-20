974ebe5f8dbd9f2e5abdd5b62016a0d086a34adc
Buntu Cossie <buntucossie@gmail.com>
2022-04-20 10:40:13 +0200

    added edit functionality

--- Controllers/PublisherController.cs
+++ Controllers/PublisherController.cs
@@ -32,5 +32,23 @@ namespace IGDB.Controllers
             }
             return View(publisher);
         }
+        public IActionResult Edit(int id)
+        {
+            var publisher = this._db.Publishers.Find(id);
+            if (publisher == null) return NotFound();
+            return View(publisher);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                this._db.Publishers.Update(publisher);
+                this._db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(publisher);
+        }
     }
 }
