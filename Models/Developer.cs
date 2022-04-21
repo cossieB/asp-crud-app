5bf588d368e586d05a6a5e660405817ca837bb72
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-04-21 17:19:42 +0200

    Added Games Model

--- Models/Developer.cs
+++ Models/Developer.cs
@@ -5,12 +5,13 @@ namespace IGDB.Models
     public class Developer
     {
         [Key]
-        public int Id { get; set; }
+        public int DeveloperId { get; set; }
         [Required]
         public string Name { get; set; }
         public string Logo { get; set; }
         public string? Summary { get; set; }
         public string? Location { get; set; }
         public string? Country { get; set; }
+        public List<Game>? Games { get; set; }
     }
 }
---
+++ Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IGDB.Models
+{
+    public class Game
+    {
+        [Key]
+        public int GameId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public Developer Developer { get; set; }
+        public int DeveloperId { get; set; }
+        public Publisher Publisher { get; set; }
+        public int PublisherId { get; set; }
+
+    }
+}
