9e7dc958ebd7dde5a2f08f816e2226ab73a429de
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-04-23 15:21:12 +0200

    added search

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using IGDB.Data;
 using IGDB.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace IGDB.Controllers
 {
@@ -24,7 +26,25 @@ namespace IGDB.Controllers
             obj.Developers = this._db.Developers.Take(6).ToList();
             return View(obj);
         }
+        [HttpPost]
+        public IActionResult Search(string searchTerm)
+        {
+            var obj = new HomeModel();
 
+            var gameQuery = from game in _db.Games
+                            where EF.Functions.Like(game.Title, $"%{searchTerm}%")
+                            select game;
+            var devQuery = from dev in _db.Developers
+                            where EF.Functions.Like(dev.Name, $"%{searchTerm}%")
+                            select dev;
+            var pubQuery = from pub in _db.Publishers
+                            where EF.Functions.Like(pub.Name, $"%{searchTerm}%")
+                            select pub;
+            obj.Games = gameQuery.ToList();
+            obj.Developers = devQuery.ToList();
+            obj.Publishers = pubQuery.ToList();
+            return View(obj);
+        }
         public IActionResult Privacy()
         {
             return View();
