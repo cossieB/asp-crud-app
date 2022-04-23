a3806cedf80f388dbfbbf91288df7a85b19b7edf
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-04-23 15:55:12 +0200

    minor touchups

---
+++ Controllers/PlatformController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IGDB.Controllers
+{
+    public class PlatformController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
