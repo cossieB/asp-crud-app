479464a9b1d9353d6ae7cde7b3616479daf9ccc5
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-04-23 14:00:18 +0200

    changed home page

---
+++ Models/HomeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IGDB.Models
+{
+    public class HomeModel
+    {
+        public List<Game> Games { get; set; }
+        public List<Developer> Developers { get; set; }
+        public List <Publisher> Publishers { get; set; }
+    }
+}
